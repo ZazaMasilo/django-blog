539c600ea53ceaed82a34396e7d69e9b7c4087ff
Zandile Masilo <zandimasilo@gmail.com>
2024-11-24 18:17:01 +0000

    add collaboration forms

--- blog/views.py
+++ blog/views.py
@@ -30,9 +30,10 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-    if request.method == "POST":
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
+    if request.method == "POST": 
+         print("Received a POST request")
+         comment_form = CommentForm(data=request.POST) 
+         if comment_form.is_valid():
            comment = comment_form.save(commit=False)
            comment.author = request.user
            comment.post = post
@@ -43,7 +44,8 @@ def post_detail(request, slug):
     )
     
     comment_form = CommentForm()
-
+     
+    print("About to render template")
     return render(
         request,
         "blog/post_detail.html",
