6d5f12fb1699cf96b5d121277223072e4641a357
Zandile Masilo <zandimasilo@gmail.com>
2024-11-24 10:36:44 +0000

    changed debug to false

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-zazamasilo-djangoblog-eiksylcfhfo.ws.codeinstitute-ide.net','.herokuapp.com','8000-zazamasilo-djangoblog-mf84p82b55z.ws.codeinstitute-ide.net']
 
